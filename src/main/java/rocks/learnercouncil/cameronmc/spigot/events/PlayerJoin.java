2f2d6585015bab004ca0492c5f7f512e9a9653cd
H2ofiremaster <h2ofiremaster@gmail.com>
2022-07-28 18:00:45 -0700

    Changed plugin version and changed the logging level of debug messages.

--- src/main/java/rocks/learnercouncil/cameronmc/spigot/events/PlayerJoin.java
+++ src/main/java/rocks/learnercouncil/cameronmc/spigot/events/PlayerJoin.java
@@ -5,6 +5,7 @@ import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import rocks.learnercouncil.cameronmc.spigot.CameronMC;
@@ -17,13 +18,15 @@ import java.util.Map;
 public class PlayerJoin implements Listener {
 
     private static final Map<Player, Location> queuedPlayers = new HashMap<>();
+    private static final CameronMC plugin = CameronMC.getInstance();
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerJoin(PlayerJoinEvent e) {
+        plugin.getLogger().info("Player join event fired!");
         //Handle join commmand
         Player player = e.getPlayer();
         if(queuedPlayers.containsKey(player)) {
-            CameronMC.getInstance().getLogger().fine("Teleporting player " + player.getName() + " to " + queuedPlayers.get(player));
+            plugin.getLogger().info("Teleporting player " + player.getName() + " to " + queuedPlayers.get(player));
             player.teleport(queuedPlayers.get(player));
             queuedPlayers.remove(player);
         }
@@ -37,6 +40,6 @@ public class PlayerJoin implements Listener {
 
     public static void queueTeleport(Player player, Location location) {
         queuedPlayers.put(player, location);
-        CameronMC.getInstance().getLogger().fine("Queueing teleport of player " + player.getName() + " to location " + location);
+        plugin.getLogger().info("Queueing teleport of player " + player.getName() + " to location " + location);
     }
 }
