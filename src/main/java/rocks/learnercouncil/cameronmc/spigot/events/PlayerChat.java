44f14716dade518c96550c7313da5d0d54213bca
H2ofiremaster <h2ofiremaster@gmail.com>
2022-08-10 11:41:14 -0700

    Fixed some formatting issues

--- src/main/java/rocks/learnercouncil/cameronmc/spigot/events/PlayerChat.java
+++ src/main/java/rocks/learnercouncil/cameronmc/spigot/events/PlayerChat.java
@@ -1,6 +1,7 @@
 package rocks.learnercouncil.cameronmc.spigot.events;
 
 import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -12,8 +13,11 @@ public class PlayerChat implements Listener {
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent e) {
         if(chatStyle == null) chatStyle = "&f<%player_name%> ";
-        String msg = PlaceholderAPI.setPlaceholders(e.getPlayer(), chatStyle) + e.getMessage();
-        PluginMessageHandler.sendPluginMessage(e.getPlayer(), "chat_message", e.getPlayer().getUniqueId().toString(), msg);
+        String msg =
+                ChatColor.translateAlternateColorCodes( '&',
+                PlaceholderAPI.setPlaceholders(e.getPlayer(), chatStyle)
+                ) + e.getMessage();
+        PluginMessageHandler.sendPluginMessage(e.getPlayer(), "chat-message", e.getPlayer().getUniqueId().toString(), msg);
         e.setCancelled(true);
     }
 }
