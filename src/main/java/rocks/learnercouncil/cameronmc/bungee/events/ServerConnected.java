1d1fa43cbd50ee6abdfceb524d7b9769a5a32c94
H2ofiremaster <h2ofiremaster@gmail.com>
2022-08-11 23:53:08 -0700

    Replaced 'ConnectionQueue' with Runnable as they're functionally identical

--- src/main/java/rocks/learnercouncil/cameronmc/bungee/events/ServerConnected.java
+++ src/main/java/rocks/learnercouncil/cameronmc/bungee/events/ServerConnected.java
@@ -5,7 +5,6 @@ import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import rocks.learnercouncil.cameronmc.bungee.CameronMC;
-import rocks.learnercouncil.cameronmc.bungee.util.ConnectionQueue;
 
 import java.util.HashMap;
 
@@ -13,14 +12,14 @@ public class ServerConnected implements Listener {
 
     private static final CameronMC plugin = CameronMC.getInstance();
 
-    public static HashMap<ProxiedPlayer, ConnectionQueue> queuedPlayers = new HashMap<>();
+    public static HashMap<ProxiedPlayer, Runnable> queuedPlayers = new HashMap<>();
 
     @EventHandler
     public void onServerConnected(ServerConnectedEvent e) {
         for(ProxiedPlayer p : queuedPlayers.keySet()) {
             if(p.getUniqueId().equals(e.getPlayer().getUniqueId())) {
                 plugin.getLogger().fine("(ServerConnected) Firing queued action for player " + p.getName() + ".");
-                queuedPlayers.get(p).fireQueued();
+                queuedPlayers.get(p).run();
                 queuedPlayers.remove(p);
             }
         }
