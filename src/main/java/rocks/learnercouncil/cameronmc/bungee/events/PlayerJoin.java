7368b2a42a383131df60c491439003685e60073b
H2ofiremaster <h2ofiremaster@gmail.com>
2023-02-28 09:56:58 -0800

    Added 'NavigatorLocation' class

--- src/main/java/rocks/learnercouncil/cameronmc/bungee/events/PlayerJoin.java
+++ src/main/java/rocks/learnercouncil/cameronmc/bungee/events/PlayerJoin.java
@@ -5,22 +5,22 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+import rocks.learnercouncil.cameronmc.bungee.CameronMC;
 import rocks.learnercouncil.cameronmc.bungee.util.ChatHandler;
 
 public class PlayerJoin implements Listener {
 
+    public static final CameronMC plugin = CameronMC.getInstance();
+
     @EventHandler
     public void onPlayerJoin(ServerConnectEvent e) {
-        if(e.getReason().equals(ServerConnectEvent.Reason.JOIN_PROXY)) {
-            ProxiedPlayer p = e.getPlayer();
-            ChatHandler.clearMessageHistory(p);
+        if(!e.getReason().equals(ServerConnectEvent.Reason.JOIN_PROXY)) return;
+        ProxiedPlayer p = e.getPlayer();
+        ChatHandler.clearMessageHistory(p);
 
-            if(e.getPlayer().getPendingConnection().getVirtualHost().getHostString().equals("play.learnercouncil.rocks")) {
-                p.sendMessage(new ComponentBuilder("§4§lATTENTION! §c§lYou connected to the server using §f§lplay.learnercouncil.rocks§c§l, that server address works for now, but it won't for long! In the future, please connect using §f§lmc.learnercouncil.rocks§c§l, Thank you.").create());
-            } else
-            if(!e.getPlayer().getPendingConnection().getVirtualHost().getHostString().contains("learnercouncil.rocks")) {
-                p.sendMessage(new ComponentBuilder("§4§lATTENTION! §c§lYou connected to the server using a unknown address, that address is not controlled by us and may be unsafe. In the future, please connect using §f§lmc.learnercouncil.rocks§c§l, Thank you.").create());
-            }
-        }
+        if(e.getPlayer().getPendingConnection().getVirtualHost().getHostString().equals("play.learnercouncil.rocks"))
+            p.sendMessage(new ComponentBuilder("§4§lATTENTION! §c§lYou connected to the server using §f§lplay.learnercouncil.rocks§c§l, that server address works for now, but it won't for long! In the future, please connect using §f§lmc.learnercouncil.rocks§c§l, Thank you.").create());
+        else if(!e.getPlayer().getPendingConnection().getVirtualHost().getHostString().contains("learnercouncil.rocks"))
+            p.sendMessage(new ComponentBuilder("§4§lATTENTION! §c§lYou connected to the server using a unknown address, that address is not controlled by us and may be unsafe. In the future, please connect using §f§lmc.learnercouncil.rocks§c§l, Thank you.").create());
     }
 }
---
+++ src/main/java/rocks/learnercouncil/cameronmc/bungee/util/NavigatorLocation.java
@@ -0,0 +1,69 @@
+package rocks.learnercouncil.cameronmc.bungee.util;
+
+import lombok.Getter;
+import rocks.learnercouncil.cameronmc.bungee.CameronMC;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Optional;
+
+public class NavigatorLocation {
+
+    private static final CameronMC plugin = CameronMC.getInstance();
+    private static @Getter HashMap<String, NavigatorLocation> locations = new HashMap<>();
+
+    public static Optional<NavigatorLocation> get(String name) {
+        return Optional.ofNullable(locations.get(name));
+    }
+
+    private final @Getter String server, world, x, y, z, pitch, yaw;
+
+    public NavigatorLocation(String server, String world, String x, String y, String z, String pitch, String yaw) {
+        this.server = server;
+        this.world = world;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.pitch = pitch;
+        this.yaw = yaw;
+    }
+
+    public static void initialize() {
+        locations = new LinkedHashMap<>();
+        Collection<String> locationNames = plugin.navigatorCfg.getConfig().getKeys();
+        for(String key : locationNames) {
+            locations.put(key, new NavigatorLocation(
+                     plugin.navigatorCfg.getConfig().getString(key + ".server"),
+                     plugin.navigatorCfg.getConfig().getString(key + ".world"),
+                     getConfigString(key, "x", false),
+                     getConfigString(key, "y", false),
+                     getConfigString(key, "z", false),
+                     getConfigString(key, "pitch", true),
+                     getConfigString(key, "yaw", true)
+            ));
+        }
+    }
+
+    private static String getConfigString(String key, String value, boolean isFloat) {
+        return checkParseable(plugin.navigatorCfg.getConfig().getString(key + "." + value), key, isFloat);
+    }
+
+    private static String checkParseable(String input, String key, boolean isFloat) {
+        if(isFloat) {
+            try {
+                Float.parseFloat(input);
+            } catch (NumberFormatException ignored) {
+                plugin.getLogger().warning("Key '" + key + "' (Value: " + input + ") not parseable as float.");
+            }
+            return input;
+        }
+        try {
+            Double.parseDouble(input);
+        } catch (NumberFormatException ignored) {
+            plugin.getLogger().warning("Key '" + key + "' (Value: " + input + ") not parseable as double.");
+        }
+        return input;
+    }
+
+}
