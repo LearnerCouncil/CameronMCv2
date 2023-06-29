8bc4219974a9365ab85271226996c56b3e03c2db
H2ofiremaster <h2ofiremaster@gmail.com>
2023-06-28 20:37:59 -0700

    Fixed "Unknown address" error when bedrock edition players join.

--- src/main/java/rocks/learnercouncil/cameronmc/bungee/events/PlayerJoin.java
+++ src/main/java/rocks/learnercouncil/cameronmc/bungee/events/PlayerJoin.java
@@ -1,6 +1,7 @@
 package rocks.learnercouncil.cameronmc.bungee.events;
 
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ServerConnectEvent;
@@ -9,6 +10,8 @@ import net.md_5.bungee.event.EventHandler;
 import rocks.learnercouncil.cameronmc.bungee.CameronMC;
 import rocks.learnercouncil.cameronmc.bungee.util.NavigatorLocation;
 import rocks.learnercouncil.cameronmc.bungee.util.PluginMessageHandler;
+
+import java.util.Optional;
 
 public class PlayerJoin implements Listener {
 
@@ -20,10 +23,11 @@ public class PlayerJoin implements Listener {
         ProxiedPlayer p = e.getPlayer();
         sendToHub(p, e.getTarget());
 
-        if(e.getPlayer().getPendingConnection().getVirtualHost().getHostString().equals("play.learnercouncil.rocks"))
+        if(p.getPendingConnection().getVirtualHost().getHostString().equals("play.learnercouncil.rocks"))
             p.sendMessage(new ComponentBuilder("§4§lATTENTION! §c§lYou connected to the server using §f§lplay.learnercouncil.rocks§c§l, that server address works for now, but it won't for long! In the future, please connect using §f§lmc.learnercouncil.rocks§c§l, Thank you.").create());
-        else if(!e.getPlayer().getPendingConnection().getVirtualHost().getHostString().contains("learnercouncil.rocks"))
+        else if(!p.getPendingConnection().getVirtualHost().getHostString().contains("learnercouncil.rocks") && !p.getName().startsWith("+"))
             p.sendMessage(new ComponentBuilder("§4§lATTENTION! §c§lYou connected to the server using a unknown address, that address is not controlled by us and may be unsafe. In the future, please connect using §f§lmc.learnercouncil.rocks§c§l, Thank you.").create());
+
     }
 
     private void sendToHub(ProxiedPlayer player, ServerInfo targetServer) {
