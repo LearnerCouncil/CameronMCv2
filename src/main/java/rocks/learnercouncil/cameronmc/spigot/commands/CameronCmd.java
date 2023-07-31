a078d6c611f07a5c79a9a0442eebf8198d879983
H2ofiremaster <h2ofiremaster@gmail.com>
2023-07-31 07:21:50 -0700

    Changed flight speed range, and changed variable names

--- src/main/java/rocks/learnercouncil/cameronmc/spigot/commands/CameronCmd.java
+++ src/main/java/rocks/learnercouncil/cameronmc/spigot/commands/CameronCmd.java
@@ -8,8 +8,6 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
-import rocks.learnercouncil.cameronmc.common.CommandResult;
-import rocks.learnercouncil.cameronmc.spigot.CameronMC;
 import rocks.learnercouncil.cameronmc.spigot.PluginMessageHandler;
 
 import java.util.ArrayList;
@@ -20,7 +18,6 @@ import java.util.Objects;
 import static rocks.learnercouncil.cameronmc.common.CommandResult.*;
 
 public class CameronCmd implements CommandExecutor, TabCompleter {
-    private static final CameronMC plugin = CameronMC.getInstance();
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, Command cmd, @NotNull String label, @NotNull String[] args) {
@@ -29,50 +26,50 @@ public class CameronCmd implements CommandExecutor, TabCompleter {
             sender.spigot().sendMessage(needsPlayer(label));
             return true;
         }
-        Player p = (Player) sender;
-        if (!p.hasPermission("cameron.commands.cameron")) {
-            p.spigot().sendMessage(NO_PERMISSION);
+        Player player = (Player) sender;
+        if (!player.hasPermission("cameron.commands.cameron")) {
+            player.spigot().sendMessage(NO_PERMISSION);
             return true;
         }
         if (args.length < 1) {
-            p.spigot().sendMessage(NOT_ENOUGH_ARGS);
+            player.spigot().sendMessage(NOT_ENOUGH_ARGS);
             return true;
         }
         if (args[0].equalsIgnoreCase("setnavigator")) {
             if (args.length != 2) {
-                p.spigot().sendMessage(MUST_SPECIFY_LOCATION);
+                player.spigot().sendMessage(MUST_SPECIFY_LOCATION);
                 return true;
             }
-            Location l = p.getLocation();
-            PluginMessageHandler.sendPluginMessage(p, "set-navigator",
+            Location l = player.getLocation();
+            PluginMessageHandler.sendPluginMessage(player, "set-navigator",
                     args[1],
-                    p.getUniqueId().toString(),
+                    player.getUniqueId().toString(),
                     Objects.requireNonNull(l.getWorld()).getName(),
                     String.valueOf(l.getX()),
                     String.valueOf(l.getY()),
                     String.valueOf(l.getZ()),
                     String.valueOf(l.getPitch()),
                     String.valueOf(l.getYaw()));
-            p.spigot().sendMessage(locationSet(Objects.requireNonNull(l.getWorld()).getName(),
+            player.spigot().sendMessage(locationSet(Objects.requireNonNull(l.getWorld()).getName(),
                     String.valueOf(l.getX()), String.valueOf(l.getY()), String.valueOf(l.getZ()),
                     String.valueOf(l.getYaw()), String.valueOf(l.getPitch())));
             return true;
         } else if (args[0].equalsIgnoreCase("flyspeed")) {
             if (args.length != 2) {
-                p.spigot().sendMessage(MUST_SPECIFY_SPEED);
+                player.spigot().sendMessage(MUST_SPECIFY_SPEED);
                 return true;
             }
             float speed;
             try {
                 speed = Float.parseFloat(args[1]);
             } catch (NumberFormatException e) {
-                p.spigot().sendMessage(invalidNumber(args[1]));
+                player.spigot().sendMessage(invalidNumber(args[1]));
                 return true;
             }
-            p.setFlySpeed(speed);
-            p.spigot().sendMessage(speedSet(args[1]));
+            player.setFlySpeed(speed / 10);
+            player.spigot().sendMessage(speedSet(args[1]));
         }
-        p.spigot().sendMessage(INVALID_ARGS);
+        player.spigot().sendMessage(INVALID_ARGS);
         return true;
     }
 
@@ -80,11 +77,9 @@ public class CameronCmd implements CommandExecutor, TabCompleter {
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
         List<String> arguments = new ArrayList<>();
         List<String> completions = new ArrayList<>();
-        if (args.length == 1) {
-                if(sender.hasPermission("cameron.commands.cameron")) {
-                    arguments.add("setnavigator");
-                    arguments.add("flyspeed");
-                }
+        if (args.length == 1 && sender.hasPermission("cameron.commands.cameron")) {
+            arguments.add("setnavigator");
+            arguments.add("flyspeed");
         }
         if(args.length > 0) {
             StringUtil.copyPartialMatches(args[args.length - 1], arguments, completions);
